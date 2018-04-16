88b9e897a1cdc724271a2b1e61a269ee127b765c
johngalay <jadgalay@gmail.com>
2018-04-16 19:03:24 +0800

    misc0

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -6,11 +6,20 @@ public class PlayerNetwork : MonoBehaviour {
 	[SerializeField] private Transform playerCamera;
 	[SerializeField] private MonoBehaviour[] playerControlSripts;
 
+	private delegate void UpdateUI(int newHealth);
+	private event UpdateUI updateUI;
+
 	private PhotonView photonView;
 	public int playerHealth = 90;
 
 	private void Start() {
 		photonView = GetComponent<PhotonView>();
+
+		// Update the event with a method
+		updateUI += FindObjectOfType<PlayerUI>().UpdateUI;
+		//myRigidbody.gameObject.GetComponent<PlayerUI>().m_UIText = textUI.GetComponent<Text>();
+		//this.GetComponentInParent<PlayerUI>().m_UIText = this.GetComponentInParent<GeneratePlayerUI>().textUI.GetComponent<Text>();
+
 		Initialize();
 	}
 
@@ -25,6 +34,8 @@ public class PlayerNetwork : MonoBehaviour {
 				m.enabled = false;
 			}
 		}
+		// Update UI on start
+		updateUI(playerHealth);
 	}
 
 	private void Update() {
@@ -34,18 +45,22 @@ public class PlayerNetwork : MonoBehaviour {
 
 		if(Input.GetKeyDown(KeyCode.E)){
 			playerHealth = playerHealth - 5;
+			updateUI(playerHealth);
 		}
 	}
 
 	private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
 		// send data
 		if(stream.isWriting) {
+			Debug.Log("Writing");
 			stream.SendNext(playerHealth);
 		}
 
 		// receive data
 		else if(stream.isReading) {
+			Debug.Log("Reading");
 			playerHealth = (int)stream.ReceiveNext();
+			updateUI(playerHealth);
 		}
 	}
 }
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour {
+
+	[SerializeField] public Text m_UIText;
+
+	public void UpdateUI(int newHealth) {
+		Debug.Log("Passed: " + newHealth);
+		m_UIText.text = newHealth.ToString();
+	}
+
+}
